e78253829d1d2c12c3983bc31aad619ba8cd5627
Rangel Netto <rangel@juno.com.br>
2020-07-02 12:21:11 -0300

    SignUp: Component flow

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import {
+  FiArrowLeft, FiMail, FiUser, FiLock,
+} from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => (
+const SignUp: React.FC = () => (
   <Container>
+    <Background />
     <Content>
       <img src={logoImg} alt="GoBarber" />
       <form>
-        <h1>Faça seu Logon</h1>
+        <h1>Faça seu Cadastro</h1>
 
+        <Input icon={FiUser} name="user" placeholder="Name" />
         <Input icon={FiMail} name="email" placeholder="E-mail" />
         <Input icon={FiLock} name="password" type="password" placeholder="senha" />
-        <Button type="submit">Entrar</Button>
-
-        <a href="forgot">Esqueci minha senha</a>
+        <Button type="submit">Cadastrar</Button>
       </form>
 
-      <a href="create">
-        <FiLogIn />
-        Criar Conta
+      <a href="back">
+        <FiArrowLeft />
+        Voltar
       </a>
     </Content>
-    <Background />
   </Container>
 );
 
-export default SignIn;
+export default SignUp;
